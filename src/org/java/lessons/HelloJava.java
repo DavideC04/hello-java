59762f15baeda9a0e29c26c3261056ea22026c48
Davide Cangiano <cangiano.0497@gmail.com>
2023-07-11 12:49:13 +0200

    Add first class and printed on terminal

---
+++ src/org/java/lessons/HelloJava.java
@@ -0,0 +1,10 @@
+package org.java.lessons;
+
+public class HelloJava {
+// tutto il codice va scritto all'interno delle parentesi graffe
+    public static void main(String[] args) {
+        System.out.println("This is my first Java program");
+        System.out.println("Bye Bye");
+
+}
+}
