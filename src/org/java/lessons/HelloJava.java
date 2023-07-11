bbceb77551349092aa17d1a729506d1e6e308dd4
Davide Cangiano <cangiano.0497@gmail.com>
2023-07-11 12:59:18 +0200

    Add "I'm back!"

--- src/org/java/lessons/HelloJava.java
+++ src/org/java/lessons/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("This is my first Java program");
         System.out.println("Bye Bye");
+        System.out.println("I'm back!");
 
     }
 }
